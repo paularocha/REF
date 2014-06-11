4ed06b058160eab379f1e7b7e498569798cedf82
AndreRamos7 <andre-rammos@hotmail.com>
2014-06-11 11:42:59 -0300

    Criado um modelo de Bean e DAO dos eventos do google agenda.

--- src/java/dao/EventoGoogleDAO.java
+++ src/java/dao/EventoGoogleDAO.java
@@ -1,17 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package dao;
 
-/**
- *
- * @author Anderson
- */
+import beans.EventoGoogleBean;
+import java.util.ArrayList;
+
+
 public class EventoGoogleDAO {
+    private String agendaId;
+    private ArrayList<EventoGoogleBean> eventList;
+    private ArrayList<Object> eventListDay;
+
+    public EventoGoogleDAO(String agendaId) {        
+        this.agendaId = agendaId;
+    }
+
     
+    public ArrayList<EventoGoogleBean> listarTodosEventos(){    
+        eventList = new ArrayList<>();
+        return eventList;
+    }
+    public ArrayList<EventoGoogleBean> listarEventosDoDia(String dia){
+        eventListDay = new ArrayList<>();
+        return eventList;
+    }
+    
+    public void deletarEvento(String eventoId){}
+    
+    public String criarEvento(String sumario, String motivo, String startDateTime, String endDateTime){
+        
+        return "idEventCreate";
+    }
+    
+    public void atualizarEvento(String eventoId){}
+    
+    
+    
+    
+    
+    
+    public String getAgendaId() {
+        return agendaId;
+    }
+
+    public void setAgendaId(String agendaId) {
+        this.agendaId = agendaId;
+    }
     
     
 }
